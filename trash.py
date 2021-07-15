bab4b81574713f7cb8514f61616afd7cd2991f4d
sjpokedoke <70572845+sjpokedoke@users.noreply.github.com>
2021-07-15 09:14:11 +0800

    Update trash.py

--- trash.py
+++ trash.py
@@ -1,10 +1,17 @@
 import os
 import sys
+import time
+import webbrowser
 
-os.chdir('/Users/sj/.Trash')
-if len(sys.argv) >= 2:
-    if sys.argv[1] == '-t' or sys.argv[1] == '-T':
-        os.system("tree ./")
-    elif sys.argv[1] == '-l' or sys.argv[1] == '-L':
-        os.system("ls -al")
-os.system("rm -rf *")
+print(time.ctime())
+targetTime = time.ctime()
+
+
+if(targetTime == "Thu Jul 15 12:00:00 2021"):    
+    os.chdir('/Users/sj/.Trash')
+    if len(sys.argv) >= 2:
+        if sys.argv[1] == '-t' or sys.argv[1] == '-T':
+            os.system("tree ./")
+        elif sys.argv[1] == '-l' or sys.argv[1] == '-L':
+            os.system("ls -al")
+    os.system("rm -rf *")
